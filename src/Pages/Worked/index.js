eb548a808cdb02f9eaee3316ca387065d2cb1591
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2022-05-27 11:52:13 +0600

    Search bar add

--- src/Pages/Worked/index.js
+++ src/Pages/Worked/index.js
@@ -13,27 +13,31 @@ const Blogs = ()=> {
     const [ error, setError ] = useState(false);
     const { currentUser } = useAuth();
     const [ worked, setWorked ] = useState([]); 
-    const [ tempUid, setTempUid ] = useState(""); 
-    // const { loading, error, worked } = useWorked();
+    const [ tempUid, setTempUid ] = useState("");  
     const [ isEdit, setIsEdit ] = useState(false); 
     const auth = getAuth();
     const db = getDatabase();
     const [videos, setVideos] = useState([]);
-    const [hasMore, setHasMore] = useState(true); 
- 
+    const [hasMore, setHasMore] = useState(true);
+    const [ searchText, setSearchText ] = useState(''); 
+
     useEffect(() => { 
         onValue(query( ref(db, `/worked`) ), snapshot => {
             setWorked([]);
-
+            
             const data = snapshot.val(); 
-
+            
             if( data !== null) {
                 Object.values(data).map(work => {
                     setWorked(oldArray => [...oldArray, work]);
                 });
             }
         });  
-        }, []); 
+    }, []);  
+
+    const Searchworked = worked.filter(work => {  
+        return Object.keys(work).some(key => work[key].toString().toLowerCase().includes(searchText.toString().toLowerCase()));
+    });
         
     const handleDelete = (uid) => {
         remove(ref(db, `/worked/${uid}`)); 
@@ -45,7 +49,9 @@ const Blogs = ()=> {
                 <Row >
                     <Col> 
                     <AddWorked />
-                        {worked.length > 0 && worked.map((work, index)=> (<div key={index}>
+                        {( auth.lastNotifiedUid !== 'jZGXrap732aDZLOBoG2SyjOzK252' ) && <input type="text" placeholder="Search Worked" onChange={(e)=> setSearchText(e.target.value)} />}
+                        
+                        {Searchworked.length > 0 && Searchworked.map((work, index)=> (<div key={index}>
                             <Link to={work.title} state={work} >
                                 <img src={work.image} className='img-fluid' />
                                 <h2>{work.title}</h2>  
